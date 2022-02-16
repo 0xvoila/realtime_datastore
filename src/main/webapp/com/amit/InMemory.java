be3dadce7b1080e974bde410f7bb23ae8d678916
Amit Aggarwal <amit.aggarwal@unbxd.com>
2022-02-16 09:26:19 +0530

    Working model of real time

---
+++ src/main/webapp/com/amit/InMemory.java
@@ -0,0 +1,83 @@
+package com.amit;
+
+import java.util.*;
+
+public class InMemory<K, V>{
+
+    final static int INIT_CAPACITY = 1000;
+
+    ArrayList<LinkedList<HashMap<K, V>>> hashTable = new ArrayList<>();
+
+    public InMemory(){
+        createHashTable(this.hashTable);
+    }
+
+    private void createHashTable(ArrayList<LinkedList<HashMap<K, V>>> hashTable){
+
+        for(int i = 0; i < InMemory.INIT_CAPACITY; i++){
+            LinkedList<HashMap<K,V>> ll = new LinkedList<>();
+            hashTable.add(ll);
+        }
+    }
+
+    public void put(K key, V value){
+
+        int hashCode = Math.abs(key.hashCode());
+        int bucket = hashCode % hashTable.size();
+
+        LinkedList<HashMap<K,V>> ll = hashTable.get(bucket);
+
+        HashMap<K,V> keyValue = new HashMap<>();
+        keyValue.put(key, value);
+        ll.add(keyValue);
+    }
+
+    public V get(K key){
+
+        boolean isFound = false;
+        V value = null;
+        int hashCode = Math.abs(key.hashCode());
+        int bucket = hashCode % hashTable.size();
+
+        LinkedList<HashMap<K,V>> ll = hashTable.get(bucket);
+
+        for (HashMap<K, V> keyValue : ll) {
+            for(HashMap.Entry<K, V> entry: keyValue.entrySet()){
+                if(entry.getKey().equals(key)){
+                    value = entry.getValue();
+                    isFound = true;
+                }
+            }
+            if (isFound) break;
+        }
+        return value;
+    }
+
+    public void displayHashTable(){
+
+        for (int i=0; i<hashTable.size(); i++){
+            LinkedList<HashMap<K,V>> ll = hashTable.get(i);
+            Iterator<HashMap<K,V>> it = ll.iterator();
+
+            System.out.println("Bucket is " + i);
+            while(it.hasNext()){
+                System.out.println(it.next());
+            }
+        }
+    }
+
+    public static void main(String args[]){
+
+        InMemory<Integer, Integer> myHashMap = new InMemory<>();
+
+        for(int i=0; i<2000000; i++){
+            myHashMap.put(i, i);
+        }
+//        myHashMap.displayHashTable();
+
+        Object v = myHashMap.get(100);
+        System.out.println(v);
+
+    }
+}
+
---
+++ target/realdb/com/amit/Main.java
@@ -0,0 +1,53 @@
+package com.amit;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Main extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        try{
+            InMemory<String, Object> inMemory = new InMemory<>();
+            String key = request.getParameter("key");
+            Object value = inMemory.get(key);
+
+            PrintWriter printWriter = response.getWriter();
+            printWriter.print(value);
+        }
+        catch(Exception exception){
+
+            PrintWriter printWriter = response.getWriter();
+            printWriter.print("");
+        }
+
+    }
+
+    // Method to handle POST method request.
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        try{
+            InMemory<String, Object> inMemory = new InMemory<>();
+            String key = request.getParameter("key");
+            Object value = request.getParameter("value");
+            inMemory.put(key, value);
+
+            PrintWriter printWriter = response.getWriter();
+            printWriter.print("Done");
+        }
+        catch(Exception exception){
+            PrintWriter printWriter = response.getWriter();
+            printWriter.print("Fail");
+        }
+
+    }
+
+}
